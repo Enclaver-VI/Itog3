16f37476cae0d9ec8a0e61d77072eb9a329d22cb
Шведов Вадим Игоревич <Shvedovvadim91@gmail.com>
2019-11-10 12:06:54 +0300

    Итоговая 2

---
+++ src/Itog2.java
@@ -0,0 +1,35 @@
+    import java.util.Scanner;
+
+    public class Itog2 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите размер массива: ");
+            int size = scanner.nextInt();
+            int array[] = new int[size];
+            System.out.println("Введите элементы массива:");
+
+            for (int i = 0; i < size; i++) {
+                array[i] = scanner.nextInt();
+            }
+
+            System.out.println("Введенный массив:");
+            for (int i = 0; i < size; i++) {
+                System.out.print(array[i] + " ");
+            }
+            for (int i = array.length - 1; i > 0; i--) {
+                for (int j = 0; j < i; j++) {
+                    if (array[j] > array[j + 1]) {
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
+            System.out.println("\nМассив, полученный после сортировки:");
+            for (int i = 0; i < size; i++) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+
