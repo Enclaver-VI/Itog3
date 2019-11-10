96a45bf24ce5baf0cb37742a4a08eb7ec78cb258
Шведов Вадим Игоревич <Shvedovvadim91@gmail.com>
2019-11-10 10:57:05 +0300

    Здавание 11

---
+++ src/DZ11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DZ11 {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Необходимо ввести строку:");
+            String str = scanner.nextLine();
+
+            System.out.println("Вы ввели:\n" + str);
+
+        }
+    }
+
+
+
